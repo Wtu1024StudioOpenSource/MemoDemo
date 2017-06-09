684f42a90682672f99bf17eb83f342e7cf343e33
Closer <664095924@qq.com>
2017-06-10 03:25:17 +0800

    仿照iOS9的备忘录，用到自定义cell

---
+++ 备忘录/备忘录1.1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  备忘录1.1
+//
+//  Created by yinquan on 16/11/7.
+//  Copyright © 2016年 yinquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
